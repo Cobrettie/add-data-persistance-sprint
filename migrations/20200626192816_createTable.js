6a581827ec5ee690b682c154fe194d1ec675aae2
Cobrettie <cobrettiegarner3@gmail.com>
2020-06-28 08:26:31 -0400

    configure initial migration file

--- migrations/20200626192816_createTable.js
+++ migrations/20200626192816_createTable.js
@@ -1,8 +1,36 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable('project', table => {
+    table.increments('id')
+    table.text('project_name').notNullable()
+    table.text('project_description')
+    table.bool('completed').defaultTo(false)
+  })
+    
+  await knex.schema.createTable('resource', table => {
+    table.increments('id')
+    table.string('resource_name').notNullable()
+    table.string('resource_description')
+    table.integer('project_id')
+      .references('id')
+      .inTable('project')
+      .notNullable()
+  })
+
+  await knex.schema.createTable('task', table => {
+    table.integer('id')
+    table.text('task_description').notNullable()
+    table.text('task_notes')
+    table.bool('completed').defaultTo(false)
+    table.integer('project_id')
+      .references('id')
+      .inTable('project')
+      .notNullable()
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('project')
+  await knex.schema.dropTableIfExists('resource')
+  await knex.schema.dropTableIfExists('task')
 };
