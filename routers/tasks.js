b6e8af994ff5c6c153ddf7d38eafaa8a195d6224
Cobrettie <cobrettiegarner3@gmail.com>
2020-06-28 20:09:44 -0400

    refactor tasks router

--- routers/tasks.js
+++ routers/tasks.js
@@ -18,23 +18,23 @@ router.post('/', async (req, res) => {
 })
 
 // get all tasks
-router.get('/', (req, res) => {
-  tasks.getTasks()
-    .then(tasks => {
-      res.status(201).json(tasks)
-    })
-    .catch(err => logError(err))
+router.get('/', async (req, res) => {
+  try {
+    const allTasks = await tasks.getTasks()
+    res.status(201).json(allTasks)
+  } catch (err) {
+    logError(err)
+  }
 })
 
-// get project task
-
 // get task by id
-router.get('/:id', (req, res) => {
-  tasks.getTaskById(req.params.id)
-    .then(task => {
-      res.status(200).json(task)
-    })
-    .catch(err => logError(err))
+router.get('/:id', async (req, res) => {
+  try {
+  const task = await tasks.getTaskById(req.params.id)
+    res.status(200).json(task)
+  } catch (err) {
+    logError(err)
+  }
 })
 
 module.exports = router;
